50e04e72138c88dbb9bf84b14d3f52bbe9786d78
Trung Vu <trungvu@Trungs-MacBook-Pro.local>
2019-12-18 23:47:29 +0900

    add simple server starting logic and and mux lib

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
-	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
+	"os"
 )
 
 func main() {
-	fmt.Println("Test main function")
+	router := mux.NewRouter()
+
+	port := os.Getenv("deployment_port")
+
+	if port == "" {
+		port = "8000"
+	}
+
+	_ = http.ListenAndServe(":"+port, router)
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module profiler
 go 1.13
 
 require (
+	github.com/gorilla/mux v1.6.2
 	github.com/jinzhu/gorm v1.9.11
 	github.com/joho/godotenv v1.3.0
 )
